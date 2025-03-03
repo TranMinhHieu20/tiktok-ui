0f275af93d8506e1379f17195b6ec51ff578c626
minhieu <tranhieu20034@gmail.com>
2025-03-03 13:18:21 +0700

    sidebar-part1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import Header from '~/layouts/components/Header/Header';
-import Sidebar from './Sidebar/Sidebar';
+import Sidebar from '../components/Sidebar/Sidebar';
 import styles from './DefaultLayout.module.scss';
 
 const cx = classNames.bind(styles);
